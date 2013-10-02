ee812f6d6bc91beae2c791f67e616584f6e77b36
Caleb James DeLisle <cjd@hyperboria.ca>
2013-10-01 21:27:42 -0400

    Changed inttypes/signed.h to not rely on number wrapping of signed ints.

--- inttypes/signed.h
+++ inttypes/signed.h
@@ -1,17 +1,29 @@
+#define ASSERT(exp) if (!(exp)) return 100
 #define DOIT(bits,target) \
 int main() \
 { \
   target x; \
+  target y; \
   int i; \
  \
   x = 1; \
-  for (i = 0;i < bits;++i) { \
-    if (x == 0) return 100; \
-    x += x; \
+  for (i = 0;i < bits-1;++i) { \
+    y = x; \
+    if (i >= (bits-2)) x += (x - 1); \
+      else x += x; \
+    ASSERT(y > 0); \
+    ASSERT(x > y); \
   } \
-  if (x != 0) return 100; \
-  x -= 1; \
-  if (x > 0) return 100; \
+  y = 0; \
+  x = y - 1; \
+  ASSERT(y > x); \
+  for (i = 0;i < bits-1;++i) { \
+    y = x; \
+    if (i >= (bits-2)) x += (x + 1); \
+      else x += x; \
+    ASSERT(y < 0); \
+    ASSERT(x < y); \
+  } \
  \
   return 0; \
 }
